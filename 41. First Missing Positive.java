f66e71bd1bbc6a1d5a127e2755cca5d4522ed05a
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-03-26 23:43:25 +0530

    Create 41. First Missing Positive.java

---
+++ 41. First Missing Positive.java
@@ -0,0 +1,24 @@
+//Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+//You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        while (i < n) {
+            if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+        }
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}

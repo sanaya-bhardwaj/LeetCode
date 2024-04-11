a7cce49ceeb3afe365567df9f1a6ca9b598fcdba
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-04-11 23:25:36 +0530

    Create 402. Remove K Digits.java

---
+++ 402. Remove K Digits.java
@@ -0,0 +1,30 @@
+//Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+
+class Solution {
+  public String removeKdigits(String num, int k) {
+    if (num.length() == k)
+      return "0";
+
+    StringBuilder sb = new StringBuilder();
+    LinkedList<Character> stack = new LinkedList<>();
+
+    for (int i = 0; i < num.length(); ++i) {
+      while (k > 0 && !stack.isEmpty() && stack.getLast() > num.charAt(i)) {
+        stack.pollLast();
+        --k;
+      }
+      stack.addLast(num.charAt(i));
+    }
+
+    while (k-- > 0)
+      stack.pollLast();
+
+    for (final char c : stack) {
+      if (c == '0' && sb.length() == 0)
+        continue;
+      sb.append(c);
+    }
+
+    return sb.length() == 0 ? "0" : sb.toString();
+  }
+}

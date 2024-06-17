bef75555a57ed11ae19a6e2985992bff984b40ec
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-06-17 23:32:26 +0530

    Update 633. Sum of Square Numbers.java

--- 633. Sum of Square Numbers.java
+++ 633. Sum of Square Numbers.java
@@ -2,7 +2,7 @@
 
 class Solution {
     public boolean judgeSquareSum(int c) {
-        long left = 0, right = c; 
+        long left = 0, right = (long) Math.sqrt(c); 
         while (left<=right){
             long cur = left * left + right * right;
             if(cur< c) {

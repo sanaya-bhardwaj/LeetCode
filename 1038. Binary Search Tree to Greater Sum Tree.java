7dbf3ef395fca1d6c9610e176fb00c2a22c93458
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-06-25 23:52:46 +0530

    Update 1038. Binary Search Tree to Greater Sum Tree.java

--- 1038. Binary Search Tree to Greater Sum Tree.java
+++ 1038. Binary Search Tree to Greater Sum Tree.java
@@ -2,6 +2,7 @@
 //the original key plus the sum of all keys greater than the original key in BST.
 
 //As a reminder, a binary search tree is a tree that satisfies these constraints:
+
 //The left subtree of a node contains only nodes with keys less than the node's key.
 //The right subtree of a node contains only nodes with keys greater than the node's key.
 //Both the left and right subtrees must also be binary search trees.

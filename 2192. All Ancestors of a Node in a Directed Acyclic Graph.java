8c6432b4a409193ed603168ffb262d7ed4fe4611
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-06-29 15:12:41 +0530

    Update 2192. All Ancestors of a Node in a Directed Acyclic Graph.java

--- 2192. All Ancestors of a Node in a Directed Acyclic Graph.java
+++ 2192. All Ancestors of a Node in a Directed Acyclic Graph.java
@@ -2,7 +2,7 @@
 
 //You are also given a 2D integer array edges, where edges[i] = [fromi, toi] denotes that there is a unidirectional edge from fromi to toi in the graph.
 
-Return a list answer, where answer[i] is the list of ancestors of the ith node, sorted in ascending order.
+//Return a list answer, where answer[i] is the list of ancestors of the ith node, sorted in ascending order.
 
 A node u is an ancestor of another node v if u can reach v via a set of edges.
 

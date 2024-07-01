b7f2a4aec7a5b05a61184d20872f2e6d6ba57728
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-01 23:37:58 +0530

    Update 1550. Three Consecutive Odds.java

--- 1550. Three Consecutive Odds.java
+++ 1550. Three Consecutive Odds.java
@@ -1,3 +1,5 @@
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
 public class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         for (int i = 0; i < arr.length - 2; i++) {

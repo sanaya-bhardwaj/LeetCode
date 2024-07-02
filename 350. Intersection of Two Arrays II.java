2a3c4c56700bcbd9c06a78589fb0ca3f03ad847b
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-02 23:49:18 +0530

    Update 350. Intersection of Two Arrays II.java

--- 350. Intersection of Two Arrays II.java
+++ 350. Intersection of Two Arrays II.java
@@ -1,4 +1,4 @@
-
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
 class Solution {
   public int[] intersect(int[] nums1, int[] nums2) {

33ecca34943d73af8e341ccac61216fe94ce5681
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-03 22:12:54 +0530

    Create 1509. Minimum Difference between largest and smallest value in three moves.java

---
+++ 1509. Minimum Difference between largest and smallest value in three moves.java
@@ -0,0 +1,22 @@
+You are given an integer array nums.
+
+In one move, you can choose one element of nums and change it to any value.
+
+Return the minimum difference between the largest and smallest value of nums after performing at most three moves.
+
+
+class Solution {
+    public int minDifference(int[] nums) {
+        int n = nums.length;
+        if (n < 5) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        long ans = 1L << 60;
+        for (int l = 0; l <= 3; ++l) {
+            int r = 3 - l;
+            ans = Math.min(ans, (long) nums[n - 1 - r] - nums[l]);
+        }
+        return (int) ans;
+    }
+}

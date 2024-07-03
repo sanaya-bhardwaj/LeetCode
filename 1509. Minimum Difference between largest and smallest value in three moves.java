e078313b0d95189870d258e726fcb6bb79e46be2
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-03 22:13:38 +0530

    Update 1509. Minimum Difference between largest and smallest value in three moves.java

--- 1509. Minimum Difference between largest and smallest value in three moves.java
+++ 1509. Minimum Difference between largest and smallest value in three moves.java
@@ -1,5 +1,4 @@
 //You are given an integer array nums.
-
 //In one move, you can choose one element of nums and change it to any value.
 
 //Return the minimum difference between the largest and smallest value of nums after performing at most three moves.

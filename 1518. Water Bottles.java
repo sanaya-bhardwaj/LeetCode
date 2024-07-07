4887e48ab1083a655f300f91e1f6331f0eff198e
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-07 23:39:59 +0530

    Update 1518. Water Bottles.java

--- 1518. Water Bottles.java
+++ 1518. Water Bottles.java
@@ -2,6 +2,7 @@
 //You can exchange numExchange empty water bottles from the market with one full water bottle.
 //The operation of drinking a full water bottle turns it into an empty bottle.
 //Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int ans = numBottles;

221c3d741107dc1d4ee6b59fd655a1882734b5f7
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-07 23:39:20 +0530

    Update 1518. Water Bottles.java

--- 1518. Water Bottles.java
+++ 1518. Water Bottles.java
@@ -1,4 +1,5 @@
-//There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
+//There are numBottles water bottles that are initially full of water. 
+//You can exchange numExchange empty water bottles from the market with one full water bottle.
 
 The operation of drinking a full water bottle turns it into an empty bottle.
 

d295858f322e94344839bb0b4241204757bf0bdd
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-08 23:51:09 +0530

    Update 1823. Find the winner of the Circular Game.java

--- 1823. Find the winner of the Circular Game.java
+++ 1823. Find the winner of the Circular Game.java
@@ -10,7 +10,7 @@
 //The last friend you counted leaves the circle and loses the game.
 //If there is still more than one friend in the circle, go back to step 2 starting from the friend immediately clockwise of the friend who just lost and repeat.
 //Else, the last friend in the circle wins the game.
-Given the number of friends, n, and an integer k, return the winner of the game.
+//Given the number of friends, n, and an integer k, return the winner of the game.
 
 
 class Solution {

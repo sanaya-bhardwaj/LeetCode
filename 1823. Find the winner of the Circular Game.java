f8a38b78f8fa29539e751e2dfb3c7f3bc97b5985
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-08 23:50:20 +0530

    Update 1823. Find the winner of the Circular Game.java

--- 1823. Find the winner of the Circular Game.java
+++ 1823. Find the winner of the Circular Game.java
@@ -5,7 +5,7 @@
 //The rules of the game are as follows:
 
 //Start at the 1st friend.
-Count the next k friends in the clockwise direction including the friend you started at. The counting wraps around the circle and may count some friends more than once.
+//Count the next k friends in the clockwise direction including the friend you started at. The counting wraps around the circle and may count some friends more than once.
 The last friend you counted leaves the circle and loses the game.
 If there is still more than one friend in the circle, go back to step 2 starting from the friend immediately clockwise of the friend who just lost and repeat.
 Else, the last friend in the circle wins the game.

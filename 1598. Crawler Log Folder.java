b1fd6c0d3cbd1e250444ad67379ce89a1b2e83ca
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-10 20:55:13 +0530

    Update 1598. Crawler Log Folder.java

--- 1598. Crawler Log Folder.java
+++ 1598. Crawler Log Folder.java
@@ -1,7 +1,7 @@
 //The Leetcode file system keeps a log each time some user performs a change folder operation.
 //The operations are described below:
 
-"../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+//"../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
 "./" : Remain in the same folder.
 "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
 You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.

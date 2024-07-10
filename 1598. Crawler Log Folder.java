8fdfbdfea986839f94ac3cf867620232bea7a925
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-10 20:55:03 +0530

    Update 1598. Crawler Log Folder.java

--- 1598. Crawler Log Folder.java
+++ 1598. Crawler Log Folder.java
@@ -5,7 +5,6 @@
 "./" : Remain in the same folder.
 "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
 You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
-
 //The file system starts in the main folder, then the operations in logs are performed.
 //Return the minimum number of operations needed to go back to the main folder after the change folder operations.
 

ef2ea81ed117d3d7af432bc482f5fe5b5080ec48
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-11 22:51:09 +0530

    Update 1190. Reverse Substrings Between Each Pair of Parentheses.java

--- 1190. Reverse Substrings Between Each Pair of Parentheses.java
+++ 1190. Reverse Substrings Between Each Pair of Parentheses.java
@@ -1,4 +1,4 @@
-You are given a string s that consists of lower case English letters and brackets.
+//You are given a string s that consists of lower case English letters and brackets.
 
 Reverse the strings in each pair of matching parentheses, starting from the innermost one.
 

385835dba50e3ac0dbf19e9c5cc142481b583c2e
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-12 22:36:31 +0530

    Update 1717. Maximum Score From Removing Substrings.java

--- 1717. Maximum Score From Removing Substrings.java
+++ 1717. Maximum Score From Removing Substrings.java
@@ -1,5 +1,4 @@
 //You are given a string s and two integers x and y. You can perform two types of operations any number of times.
-
 //Remove substring "ab" and gain x points.
 //For example, when removing "ab" from "cabxbae" it becomes "cxbae".
 //Remove substring "ba" and gain y points.

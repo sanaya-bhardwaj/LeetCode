551bc489fb019d13f6a1aab00dac7ad197477900
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-13 23:52:19 +0530

    Update 2751. Robot Collisions.java

--- 2751. Robot Collisions.java
+++ 2751. Robot Collisions.java
@@ -4,8 +4,8 @@
 //All robots start moving on the line simultaneously at the same speed in their given directions. If two robots ever share the same position while moving, they will collide.
 //If two robots collide, the robot with lower health is removed from the line, and the health of the other robot decreases by one. 
 //The surviving robot continues in the same direction it was going. If both robots have the same health, they are both removed from the line.
-//Your task is to determine the health of the robots that survive the collisions, in the same order that the robots were given, i.e. final heath of robot 1 (if survived), final health of robot 2 (if survived), and so on. If there are no survivors, return an empty array.
-
+//Your task is to determine the health of the robots that survive the collisions, in the same order that the robots were given, i.e. final heath of robot 1 (if survived), final health of robot 2 (if survived), and so on. 
+//If there are no survivors, return an empty array.
 //Return an array containing the health of the remaining robots (in the order they were given in the input), after no further collisions can occur.
 //Note: The positions may be unsorted.
 

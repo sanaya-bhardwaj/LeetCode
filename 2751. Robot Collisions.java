e07dbff28b2d9ac77533e714e72c6263ad930e9d
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-13 23:51:36 +0530

    Update 2751. Robot Collisions.java

--- 2751. Robot Collisions.java
+++ 2751. Robot Collisions.java
@@ -1,5 +1,6 @@
 //There are n 1-indexed robots, each having a position on a line, health, and movement direction.
-//You are given 0-indexed integer arrays positions, healths, and a string directions (directions[i] is either 'L' for left or 'R' for right). All integers in positions are unique.
+//You are given 0-indexed integer arrays positions, healths, and a string directions (directions[i] is either 'L' for left or 'R' for right). 
+//All integers in positions are unique.
 
 //All robots start moving on the line simultaneously at the same speed in their given directions. If two robots ever share the same position while moving, they will collide.
 

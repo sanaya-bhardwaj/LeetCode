df650cc7fc0759cb07924821752ca87ae67397b5
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-14 23:40:17 +0530

    Update 726. Number of Atoms.java

--- 726. Number of Atoms.java
+++ 726. Number of Atoms.java
@@ -6,8 +6,7 @@
 //For example, "H2O2He3Mg4" is also a formula.
 //A formula placed in parentheses, and a count (optionally added) is also a formula.
 //For example, "(H2O2)" and "(H2O2)3" are formulas.
-
-Return the count of all elements as a string in the following form: the first name (in sorted order), followed by its count (if that count is more than 1), followed by the second name (in sorted order), followed by its count (if that count is more than 1), and so on.
+//Return the count of all elements as a string in the following form: the first name (in sorted order), followed by its count (if that count is more than 1), followed by the second name (in sorted order), followed by its count (if that count is more than 1), and so on.
 
 The test cases are generated so that all the values in the output fit in a 32-bit integer.
 

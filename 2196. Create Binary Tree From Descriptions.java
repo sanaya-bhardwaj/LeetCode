3c14ec17af210c169301ee47defd8717e7a6a565
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-15 23:40:55 +0530

    Update 2196. Create Binary Tree From Descriptions.java

--- 2196. Create Binary Tree From Descriptions.java
+++ 2196. Create Binary Tree From Descriptions.java
@@ -1,4 +1,4 @@
-You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+//You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
 
 If isLefti == 1, then childi is the left child of parenti.
 If isLefti == 0, then childi is the right child of parenti.

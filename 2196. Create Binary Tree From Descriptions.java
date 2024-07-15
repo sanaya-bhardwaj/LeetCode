b1556630afc435277c2fe1d1081265b4370e3dde
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-15 23:40:37 +0530

    Create 2196. Create Binary Tree From Descriptions.java

---
+++ 2196. Create Binary Tree From Descriptions.java
@@ -0,0 +1,35 @@
+You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+If isLefti == 1, then childi is the left child of parenti.
+If isLefti == 0, then childi is the right child of parenti.
+Construct the binary tree described by descriptions and return its root.
+
+The test cases will be generated such that the binary tree is valid.
+
+class Solution {
+  public TreeNode createBinaryTree(int[][] descriptions) {
+    Map<TreeNode, TreeNode> childToParent = new HashMap<>();
+    Map<Integer, TreeNode> valToNode = new HashMap<>();
+
+    for (int[] d : descriptions) {
+      final int p = d[0];
+      final int c = d[1];
+      final int isLeft = d[2];
+      TreeNode parent = valToNode.getOrDefault(p, new TreeNode(p));
+      TreeNode child = valToNode.getOrDefault(c, new TreeNode(c));
+      valToNode.put(p, parent);
+      valToNode.put(c, child);
+      childToParent.put(child, parent);
+      if (isLeft == 1)
+        parent.left = child;
+      else
+        parent.right = child;
+    }
+
+    // Pick a random node and traverse upwardly.
+    TreeNode root = childToParent.keySet().iterator().next();
+    while (childToParent.containsKey(root))
+      root = childToParent.get(root);
+    return root;
+  }
+}

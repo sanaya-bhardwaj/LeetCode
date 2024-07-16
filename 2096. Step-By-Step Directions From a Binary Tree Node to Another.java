3558ff7a889c191fbc1c75533905d33a1849da9e
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-16 23:54:26 +0530

    Update 2096. Step-By-Step Directions From a Binary Tree Node to Another.java

--- 2096. Step-By-Step Directions From a Binary Tree Node to Another.java
+++ 2096. Step-By-Step Directions From a Binary Tree Node to Another.java
@@ -2,13 +2,10 @@
 //You are also given an integer startValue representing the value of the start node s, and a different integer destValue representing the value of the destination node t.
 //Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path as a string consisting of only the uppercase letters 'L', 'R', and 'U'. 
 //Each letter indicates a specific direction:
-
-'L' means to go from a node to its left child node.
-'R' means to go from a node to its right child node.
-'U' means to go from a node to its parent node.
-Return the step-by-step directions of the shortest path from node s to node t.
-
-
+//'L' means to go from a node to its left child node.
+//'R' means to go from a node to its right child node.
+//'U' means to go from a node to its parent node.
+//Return the step-by-step directions of the shortest path from node s to node t.
 
 class Solution {
   public String getDirections(TreeNode root, int startValue, int destValue) {

311c5e117a87a881aaa86945c567c3c3f91fc905
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-17 23:19:53 +0530

    Create  Code Testcase Testcase Test Result 1110. Delete Nodes And Return Forest.java

---
+++ Code Testcase Testcase Test Result 1110. Delete Nodes And Return Forest.java
@@ -0,0 +1,55 @@
+//Given the root of a binary tree, each node in the tree has a distinct value.
+
+//After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+
+//Return the roots of the trees in the remaining forest. You may return the result in any order.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean[] s = new boolean[1001];
+    private List<TreeNode> ans = new ArrayList<>();
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        for (int x : to_delete) {
+            s[x] = true;
+        }
+        if (dfs(root) != null) {
+            ans.add(root);
+        }
+        return ans;
+    }
+
+    private TreeNode dfs(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = dfs(root.left);
+        root.right = dfs(root.right);
+        if (!s[root.val]) {
+            return root;
+        }
+        if (root.left != null) {
+            ans.add(root.left);
+        }
+        if (root.right != null) {
+            ans.add(root.right);
+        }
+        return null;
+    }
+}

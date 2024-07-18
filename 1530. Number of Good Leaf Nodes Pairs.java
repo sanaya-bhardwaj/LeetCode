3b76dd411085da4d269f826cb1ec8dc31dc42b48
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-18 23:41:03 +0530

    Create 1530. Number of Good Leaf Nodes Pairs.java

---
+++ 1530. Number of Good Leaf Nodes Pairs.java
@@ -0,0 +1,36 @@
+//You are given the root of a binary tree and an integer distance. 
+//A pair of two different leaf nodes of a binary tree is said to be good if the length of the shortest path between them is less than or equal to distance.
+//Return the number of good leaf node pairs in the tree.
+
+class Solution {
+  public int countPairs(TreeNode root, int distance) {
+    dfs(root, distance);
+
+    return ans;
+  }
+
+  private int ans = 0;
+
+  private int[] dfs(TreeNode root, int distance) {
+    int[] d = new int[distance + 1]; // {distance: the number of leaf nodes}
+    if (root == null)
+      return d;
+    if (root.left == null && root.right == null) {
+      d[0] = 1;
+      return d;
+    }
+
+    int[] dl = dfs(root.left, distance);
+    int[] dr = dfs(root.right, distance);
+
+    for (int i = 0; i < distance; ++i)
+      for (int j = 0; j < distance; ++j)
+        if (i + j + 2 <= distance)
+          ans += dl[i] * dr[j];
+
+    for (int i = 0; i < distance; ++i)
+      d[i + 1] = dl[i] + dr[i];
+
+    return d;
+  }
+}

d5d038ba12198512f872dffcb2e5c2ae406f80cb
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-25 22:52:15 +0530

    Create 912. Sort an Array.java

---
+++ 912. Sort an Array.java
@@ -0,0 +1,33 @@
+//Given an array of integers nums, sort the array in ascending order and return it.
+//You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+class Solution {
+    private int[] nums;
+
+    public int[] sortArray(int[] nums) {
+        this.nums = nums;
+        quikcSort(0, nums.length - 1);
+        return nums;
+    }
+
+    private void quikcSort(int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int x = nums[(l + r) >> 1];
+        int i = l - 1, j = r + 1;
+        while (i < j) {
+            while (nums[++i] < x) {
+            }
+            while (nums[--j] > x) {
+            }
+            if (i < j) {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+            }
+        }
+        quikcSort(l, j);
+        quikcSort(j + 1, r);
+    }
+}

5e614097d9db76921a28710bf66ee97fce078bdc
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-26 23:40:55 +0530

    Create 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.java

---
+++ 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.java
@@ -0,0 +1,47 @@
+There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge between cities fromi and toi, and given the integer distanceThreshold.
+
+Return the city with the smallest number of cities that are reachable through some path and whose distance is at most distanceThreshold, If there are multiple such cities, return the city with the greatest number.
+
+Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' weights along that path.
+
+class Solution {
+   public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] distance = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(distance[i], 1000000000); // Use a large value as infinity
+            distance[i][i] = 0;
+        }
+
+        for (int[] edge : edges) {
+            distance[edge[0]][edge[1]] = edge[2];
+            distance[edge[1]][edge[0]] = edge[2];
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                }
+            }
+        }
+
+        int ans = -1;
+        int mini = Integer.MAX_VALUE;
+        int[] reachable = new int[n];
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (i != j && distance[i][j] <= distanceThreshold) {
+                    count++;
+                }
+            }
+            reachable[i] = count;
+            if (count <= mini) {
+                mini = count;
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+}

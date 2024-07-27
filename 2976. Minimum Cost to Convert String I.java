e0714028c86a7b16875c9db85cb8a35fe3b661a9
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-27 22:34:46 +0530

    Create 2976. Minimum Cost to Convert String I.java

---
+++ 2976. Minimum Cost to Convert String I.java
@@ -0,0 +1,41 @@
+You are given two 0-indexed strings source and target, both of length n and consisting of lowercase English letters. You are also given two 0-indexed character arrays original and changed, and an integer array cost, where cost[i] represents the cost of changing the character original[i] to the character changed[i].
+
+You start with the string source. In one operation, you can pick a character x from the string and change it to the character y at a cost of z if there exists any index j such that cost[j] == z, original[j] == x, and changed[j] == y.
+
+Return the minimum cost to convert the string source to the string target using any number of operations. If it is impossible to convert source to target, return -1.
+
+Note that there may exist indices i, j such that original[j] == original[i] and changed[j] == changed[i].
+
+class Solution {
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        int[][] dis = new int[26][26];
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(dis[i], Integer.MAX_VALUE);
+            dis[i][i] = 0;
+        }
+        for (int i = 0; i < cost.length; i++) {
+            dis[original[i] - 'a'][changed[i] - 'a'] = Math.min(dis[original[i] - 'a'][changed[i] - 'a'], cost[i]);
+        }
+        for (int k = 0; k < 26; k++) {
+            for (int i = 0; i < 26; i++)
+                if (dis[i][k] < Integer.MAX_VALUE) {
+                    for (int j = 0; j < 26; j++) {
+                        if (dis[k][j] < Integer.MAX_VALUE) {
+                            dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
+                        }
+                    }
+                }
+        }
+        long ans = 0L;
+        for (int i = 0; i < source.length(); i++) {
+            int c1 = source.charAt(i) - 'a';
+            int c2 = target.charAt(i) - 'a';
+            if (dis[c1][c2] == Integer.MAX_VALUE) {
+                return -1L;
+            } else {
+                ans += (long)dis[c1][c2];
+            }
+        }
+        return ans;
+    }
+}

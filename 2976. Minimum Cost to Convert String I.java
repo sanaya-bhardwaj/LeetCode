09cc1a0bda898acc1eebdc759926c9f2b569ece1
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-27 22:35:20 +0530

    Update 2976. Minimum Cost to Convert String I.java

--- 2976. Minimum Cost to Convert String I.java
+++ 2976. Minimum Cost to Convert String I.java
@@ -1,4 +1,4 @@
-You are given two 0-indexed strings source and target, both of length n and consisting of lowercase English letters. You are also given two 0-indexed character arrays original and changed, and an integer array cost, where cost[i] represents the cost of changing the character original[i] to the character changed[i].
+//You are given two 0-indexed strings source and target, both of length n and consisting of lowercase English letters. You are also given two 0-indexed character arrays original and changed, and an integer array cost, where cost[i] represents the cost of changing the character original[i] to the character changed[i].
 
 You start with the string source. In one operation, you can pick a character x from the string and change it to the character y at a cost of z if there exists any index j such that cost[j] == z, original[j] == x, and changed[j] == y.
 

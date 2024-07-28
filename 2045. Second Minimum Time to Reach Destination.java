bce31fdec65d511c2b65c42586706d727e681477
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-28 22:34:10 +0530

    Update 2045. Second Minimum Time to Reach Destination.java

--- 2045. Second Minimum Time to Reach Destination.java
+++ 2045. Second Minimum Time to Reach Destination.java
@@ -3,11 +3,9 @@
 //Every vertex pair is connected by at most one edge, and no vertex has an edge to itself. The time taken to traverse any edge is time minutes.
 //Each vertex has a traffic signal which changes its color from green to red and vice versa every change minutes. All signals change at the same time. 
 //You can enter a vertex at any time, but can leave a vertex only when the signal is green. You cannot wait at a vertex if the signal is green.
-
-The second minimum value is defined as the smallest value strictly larger than the minimum value.
-
-For example the second minimum value of [2, 3, 4] is 3, and the second minimum value of [2, 2, 4] is 4.
-Given n, edges, time, and change, return the second minimum time it will take to go from vertex 1 to vertex n.
+//The second minimum value is defined as the smallest value strictly larger than the minimum value.
+//For example the second minimum value of [2, 3, 4] is 3, and the second minimum value of [2, 2, 4] is 4.
+//Given n, edges, time, and change, return the second minimum time it will take to go from vertex 1 to vertex n.
 
 Notes:
 

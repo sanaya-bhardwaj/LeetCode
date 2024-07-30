9599717f8f8a67d719e04f46943d6093b683dc46
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-30 22:38:24 +0530

    Create 1653. Minimum Deletions to Make String Balanced.java

---
+++ 1653. Minimum Deletions to Make String Balanced.java
@@ -0,0 +1,21 @@
+//You are given a string s consisting only of characters 'a' and 'b'​​​​.
+//You can delete any number of characters in s to make s balanced. 
+//s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+//Return the minimum number of deletions needed to make s balanced.
+
+class Solution {
+    public int minimumDeletions(String s) {
+        int n = s.length();
+        int[] f = new int[n + 1];
+        int b = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (s.charAt(i - 1) == 'b') {
+                f[i] = f[i - 1];
+                ++b;
+            } else {
+                f[i] = Math.min(f[i - 1] + 1, b);
+            }
+        }
+        return f[n];
+    }
+}

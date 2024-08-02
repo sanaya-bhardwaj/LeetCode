adc20d9089c11fdc90492456bdfe5a2762da1a79
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-08-02 22:54:30 +0530

    Create 605. Can Place Flowers.java

---
+++ 605. Can Place Flowers.java
@@ -0,0 +1,22 @@
+//There are n kids with candies. 
+//You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+//Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+//Note that multiple kids can have the greatest number of candies.
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

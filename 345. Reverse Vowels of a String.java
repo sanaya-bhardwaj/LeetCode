5fb489c33b669a2d2149443131ce1ca8399c6888
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-08-03 23:30:58 +0530

    Update 345. Reverse Vowels of a String.java

--- 345. Reverse Vowels of a String.java
+++ 345. Reverse Vowels of a String.java
@@ -1,6 +1,5 @@
-Given a string s, reverse only all the vowels in the string and return it.
-
-The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+//Given a string s, reverse only all the vowels in the string and return it.
+//The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
 Approach
 Convert the input string s to an array of characters word. This step is necessary because strings are immutable in Java, and we need to modify the vowels' positions.

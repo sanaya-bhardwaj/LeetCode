56c6388d15d796104a18bcbf4d71d8fd7421fa0f
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-12-21 19:13:18 +0530

    Create 20. Valid Parentheses.java

---
+++ 20. Valid Parentheses.java
@@ -0,0 +1,23 @@
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//An input string is valid if:
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+
+public class Solution {
+    public static boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                // If the string becomes empty, it indicates all brackets are matched.
+                return s.isEmpty();
+            }
+        }
+    }
+}
+ 

724f0d375adae9d3328740f70fb33a1922916956
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-12-22 19:26:24 +0530

    Create 22. Generate Parentheses.java

---
+++ 22. Generate Parentheses.java
@@ -0,0 +1,27 @@
+//Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+class Solution {
+    List<String> res = new LinkedList<>();
+    public List<String> generateParenthesis(int n) {
+        dfs(new StringBuilder(),0,n);
+        return res; 
+    }
+    private void dfs(StringBuilder sb, int close, int n){
+        if(n==0 && close == 0){
+            res.add(sb.toString());
+            return;
+        }
+        //Add open parenthese
+        if(n>0){
+            sb.append('(');
+            dfs(sb, close +1, n-1);
+            sb.setLength(sb.length()-1);
+        }
+        //Add close parenthese
+        if(close>0){
+            sb.append(')');
+            dfs(sb, close - 1, n);
+            sb.setLength(sb.length()-1);
+        }
+    }
+}

05f23e4ff5dedd0ad7fe5709111f7638a9826516
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-12-24 22:14:32 +0530

    Create 24. Swap Nodes in Pairs.java

---
+++ 24. Swap Nodes in Pairs.java
@@ -0,0 +1,23 @@
+//Given a linked list, swap every two adjacent nodes and return its head. 
+//You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy, cur = head;
+
+        while (cur != null && cur.next != null) {
+            ListNode npn = cur.next.next;
+            ListNode second = cur.next;
+
+            second.next = cur;
+            cur.next = npn;
+            prev.next = second;
+
+            prev = cur;
+            cur = npn;
+        }
+
+        return dummy.next;        
+    }
+}

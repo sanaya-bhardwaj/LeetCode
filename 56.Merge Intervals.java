4fc52b9a21e88b5a7d621aa6bfd1e80ae7406cb4
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-01-18 19:11:30 +0530

    Create 56.Merge Intervals.java

---
+++ 56.Merge Intervals.java
@@ -0,0 +1,27 @@
+//Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+//and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        int[] prev = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if (interval[0] <= prev[1]) {
+                prev[1] = Math.max(prev[1], interval[1]);
+            } else {
+                merged.add(prev);
+                prev = interval;
+            }
+        }
+
+        merged.add(prev);
+
+        return merged.toArray(new int[merged.size()][]);         
+    }
+}
+
+ 

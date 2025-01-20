9b833fd9a1976f612c5eb058ec5a0d3a8a692fd4
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-01-20 21:01:07 +0530

    Create 68. Text Justification.java

---
+++ 68. Text Justification.java
@@ -0,0 +1,41 @@
+Given an array of strings words and a width maxWidth, format the text such that each line has exactly maxWidth characters and is fully (left and right) justified.
+You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line does not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+For the last line of text, it should be left-justified, and no extra space is inserted between words.
+Note:
+A word is defined as a character sequence consisting of non-space characters only.
+Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+The input array words contains at least one word.
+
+public class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> res = new ArrayList<>();
+        List<String> cur = new ArrayList<>();
+        int num_of_letters = 0;
+
+        for (String word : words) {
+            if (word.length() + cur.size() + num_of_letters > maxWidth) {
+                for (int i = 0; i < maxWidth - num_of_letters; i++) {
+                    cur.set(i % (cur.size() - 1 > 0 ? cur.size() - 1 : 1), cur.get(i % (cur.size() - 1 > 0 ? cur.size() - 1 : 1)) + " ");
+                }
+                StringBuilder sb = new StringBuilder();
+                for (String s : cur) sb.append(s);
+                res.add(sb.toString());
+                cur.clear();
+                num_of_letters = 0;
+            }
+            cur.add(word);
+            num_of_letters += word.length();
+        }
+
+        StringBuilder lastLine = new StringBuilder();
+        for (int i = 0; i < cur.size(); i++) {
+            lastLine.append(cur.get(i));
+            if (i != cur.size() - 1) lastLine.append(" ");
+        }
+        while (lastLine.length() < maxWidth) lastLine.append(" ");
+        res.add(lastLine.toString());
+
+        return res;
+    }
+}

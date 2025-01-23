63fa910edcdadf6eb101213324837ac87f5831e8
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-01-23 20:58:23 +0530

    Update 71. Simplify Path.java

--- 71. Simplify Path.java
+++ 71. Simplify Path.java
@@ -10,7 +10,7 @@
 //The simplified canonical path should follow these rules:
 
 //The path must start with a single slash '/'.
-Directories within the path must be separated by exactly one slash '/'.
+//Directories within the path must be separated by exactly one slash '/'.
 The path must not end with a slash '/', unless it is the root directory.
 The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
 Return the simplified canonical path.

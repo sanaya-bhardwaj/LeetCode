14cf2fbea442d67eda1ad56aca3fefd78db7ee6a
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-01-23 20:59:25 +0530

    Update 71. Simplify Path.java

--- 71. Simplify Path.java
+++ 71. Simplify Path.java
@@ -14,3 +14,22 @@
 //The path must not end with a slash '/', unless it is the root directory.
 //The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
 //Return the simplified canonical path.
+
+
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> dirOrFiles = new ArrayDeque<>();
+        for (String dirOrFile : path.split("/")) {
+            if (!dirOrFiles.isEmpty() && dirOrFile.equals("..")) {
+                dirOrFiles.removeLast();
+            } else if (!dirOrFile.equals(".") && !dirOrFile.equals("") && !dirOrFile.equals("..")) {
+                dirOrFiles.addLast(dirOrFile);
+            }
+        }
+        StringBuilder simplified_path = new StringBuilder();
+        for (String dirOrFile : dirOrFiles) {
+            simplified_path.append("/").append(dirOrFile);
+        }
+        return simplified_path.length() == 0 ? "/" : simplified_path.toString();
+    }
+}

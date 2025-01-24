f87e2d7a89e02de842c1464dcc21d77d80330c6d
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-01-24 22:15:49 +0530

    Create 74. Search a 2D Matrix.java

---
+++ 74. Search a 2D Matrix.java
@@ -0,0 +1,45 @@
+//You are given an m x n integer matrix matrix with the following two properties:
+
+//Each row is sorted in non-decreasing order.
+//The first integer of each row is greater than the last integer of the previous row.
+//Given an integer target, return true if target is in matrix or false otherwise.
+
+//You must write a solution in O(log(m * n)) time complexity.
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int top = 0;
+        int bot = matrix.length - 1;
+
+        while (top <= bot) {
+            int mid = (top + bot) / 2;
+
+            if (matrix[mid][0] < target && matrix[mid][matrix[mid].length - 1] > target) {
+                break;
+            } else if (matrix[mid][0] > target) {
+                bot = mid - 1;
+            } else {
+                top = mid + 1;
+            }
+        }
+
+        int row = (top + bot) / 2;
+
+        int left = 0;
+        int right = matrix[row].length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return false;        
+    }
+}

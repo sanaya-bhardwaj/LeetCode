f55e094df2b0e9c461d6008701d7e689237e208f
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-01-26 22:24:39 +0530

    Update 65. Valid Number.java

--- 65. Valid Number.java
+++ 65. Valid Number.java
@@ -15,4 +15,30 @@ Digits followed by a dot '.' followed by digits.
 A dot '.' followed by digits.
 An exponent is defined with an exponent notation 'e' or 'E' followed by an integer number.
 
-The digits are defined as one or more digits.
+//The digits are defined as one or more digits.
+
+class Solution {
+    public boolean isNumber(String S) {
+        boolean num = false, exp = false, sign = false, dec = false;
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (c >= '0' && c <= '9') num = true ;    
+            else if (c == 'e' || c == 'E')
+                if (exp || !num) return false;
+                else {
+                    exp = true;
+                    sign = false;
+                    num = false;
+                    dec = false;
+                }
+            else if (c == '+' || c == '-')
+                if (sign || num || dec) return false;
+                else sign = true;
+            else if (c == '.')
+                if (dec || exp) return false;
+                else dec = true;
+            else return false;
+        }
+        return num;
+    }
+}

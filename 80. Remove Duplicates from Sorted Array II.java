89b51f8ba3013602c5e1a43d1c7dd60a7dfa8638
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-01-29 20:33:51 +0530

    Update 80. Remove Duplicates from Sorted Array II.java

--- 80. Remove Duplicates from Sorted Array II.java
+++ 80. Remove Duplicates from Sorted Array II.java
@@ -15,3 +15,18 @@
 //    assert nums[i] == expectedNums[i];
 //}
 //If all assertions pass, then your solution will be accepted.
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;        
+    }
+}

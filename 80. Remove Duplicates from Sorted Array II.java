b6b16ebc9aab258f1d7098d2a20278bfe5448978
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-01-29 20:32:35 +0530

    Update 80. Remove Duplicates from Sorted Array II.java

--- 80. Remove Duplicates from Sorted Array II.java
+++ 80. Remove Duplicates from Sorted Array II.java
@@ -7,8 +7,7 @@
 //Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 //Custom Judge:
 //The judge will test your solution with the following code:
-
-int[] nums = [...]; // Input array
+//int[] nums = [...]; // Input array
 int[] expectedNums = [...]; // The expected answer with correct length
 
 int k = removeDuplicates(nums); // Calls your implementation

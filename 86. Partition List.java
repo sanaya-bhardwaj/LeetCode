abf036bb70bf9c2e387efecd113eedb7997fb500
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-02-01 22:28:07 +0530

    Update 86. Partition List.java

--- 86. Partition List.java
+++ 86. Partition List.java
@@ -1,3 +1,39 @@
 Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
 
 You should preserve the original relative order of the nodes in each of the two partitions.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode slist = new ListNode();
+        ListNode blist = new ListNode();
+        ListNode small = slist;
+        ListNode big = blist;
+
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                big.next = head;
+                big = big.next;
+            }
+
+            head = head.next;
+        }
+
+        small.next = blist.next;
+        big.next = null;
+
+        return slist.next;        
+    }
+}

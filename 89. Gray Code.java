8d8c49170976f7e0f1b70fa0538bc63be9816471
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-02-04 14:52:49 +0530

    Update 89. Gray Code.java

--- 89. Gray Code.java
+++ 89. Gray Code.java
@@ -5,3 +5,19 @@
 //The binary representation of every pair of adjacent integers differs by exactly one bit, and
 //The binary representation of the first and last integers differs by exactly one bit.
 //Given an integer n, return any valid n-bit gray code sequence.
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+
+        for(int i = 1; i <= n; i++){
+            List<Integer> rev = new ArrayList<>(list);
+            Collections.reverse(rev);
+            int val = (int)Math.pow(2,i-1);
+            for(int v : rev) list.add(v+val);
+        }
+        
+        return list;
+    }
+}

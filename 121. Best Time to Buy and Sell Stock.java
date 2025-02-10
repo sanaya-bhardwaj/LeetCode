e2338d11862a737e01656c6d95b173c2ea9884d2
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-02-10 22:18:00 +0530

    Update 121. Best Time to Buy and Sell Stock.java

--- 121. Best Time to Buy and Sell Stock.java
+++ 121. Best Time to Buy and Sell Stock.java
@@ -2,6 +2,7 @@
 //You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 //Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+
 class Solution {
     public int maxProfit(int[] prices) {
         int buy = prices[0];

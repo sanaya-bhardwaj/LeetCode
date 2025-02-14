5c6724d2f3fd1545d3803199fbcba80e5379812b
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-02-14 22:24:49 +0530

    Update 88. Merge Sorted Array.java

--- 88. Merge Sorted Array.java
+++ 88. Merge Sorted Array.java
@@ -3,8 +3,7 @@
 //The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
 //To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- //SOLUTION:
-
+//SOLUTION:
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {

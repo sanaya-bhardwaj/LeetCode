378b35a3a7ab6b5fecb996c67a3953dbc02ed071
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-02-23 20:48:43 +0530

    Update 189. Rotate Array.java

--- 189. Rotate Array.java
+++ 189. Rotate Array.java
@@ -1,5 +1,6 @@
 //Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
+
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;

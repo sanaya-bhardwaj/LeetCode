7249681c2d3b3d189325d94ac752af020e679da1
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-02-25 22:20:50 +0530

    Update 33. Search in Rotated Sorted Array.java

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -5,8 +5,6 @@
 //Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 //You must write an algorithm with O(log n) runtime complexity.
 
-
-
 class Solution {
     public int search(int[] nums, int target) {
         int left = 0;

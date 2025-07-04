bbe28dfe6009cdaad6ee39b1ca92f15f0b6d8498
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-07-04 19:00:09 +0530

    Update 9. Palindrome Number.java

--- 9. Palindrome Number.java
+++ 9. Palindrome Number.java
@@ -20,3 +20,23 @@ class Solution {
         return flag;
     }
 }
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int dup=x,lastdigit,rev=0;
+        if(x<0){
+            return false;
+        }
+        while(x!=0){
+            lastdigit=x%10;
+            x=x/10;
+            rev=rev*10+lastdigit;
+        } 
+        if(rev==dup){
+            return true;
+        }else{
+            return false;
+        }
+    }  
+}

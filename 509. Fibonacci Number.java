6b952f1e9fa70fcc4ba7dbf4851fce63b028db07
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-07-10 20:10:35 +0530

    Update 509. Fibonacci Number.java

--- 509. Fibonacci Number.java
+++ 509. Fibonacci Number.java
@@ -16,3 +16,15 @@ class Solution {
         return b;
     }
 }
+
+
+class Solution {
+    public int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        int last=fib(n-1);
+        int slast=fib(n-2);
+        return last+slast;
+    }
+}

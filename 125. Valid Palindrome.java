75308e87a9bb19f5e9a398c80f55b07d6429d311
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-09-17 17:53:00 +0530

    Update 125. Valid Palindrome.java

--- 125. Valid Palindrome.java
+++ 125. Valid Palindrome.java
@@ -25,3 +25,32 @@ class Solution {
         return f(i + 1);
     }
 }
+
+
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            // Skip non-alphanumeric
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare after converting to lowercase
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+

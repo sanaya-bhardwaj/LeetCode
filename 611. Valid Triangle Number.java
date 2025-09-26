4f7a62232348d5592dc1c366266a44d2bb677009
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-09-26 19:06:06 +0530

    Create 611. Valid Triangle Number.java

---
+++ 611. Valid Triangle Number.java
@@ -0,0 +1,22 @@
+//Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = n - 1; i >= 2; i--) {
+            int left = 0, right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    count += right - left;
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return count;
+    }
+}

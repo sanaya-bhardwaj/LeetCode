a39be7b056407bc4e879d0985f50fdb434381447
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-09-27 23:57:51 +0530

    Create 812. Largest Triangle Area.java

---
+++ 812. Largest Triangle Area.java
@@ -0,0 +1,21 @@
+//Given an array of points on the X-Y plane points where points[i] = [xi, yi], return the area of the largest triangle that can be formed by any three different points. 
+//Answers within 10-5 of the actual answer will be accepted.
+
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        double maxArea = 0.0;
+        int n = points.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    int x1 = points[i][0], y1 = points[i][1];
+                    int x2 = points[j][0], y2 = points[j][1];
+                    int x3 = points[k][0], y3 = points[k][1];
+                    double area = 0.5 * Math.abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2));
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+}

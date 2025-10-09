9d0acd15cf2b58029af5c55b416be95839685baf
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-10-09 18:58:47 +0530

    Create 3494. Find the Minimum Amount of Time to Brew Potions.java

---
+++ 3494. Find the Minimum Amount of Time to Brew Potions.java
@@ -0,0 +1,33 @@
+//You are given two integer arrays, skill and mana, of length n and m, respectively.
+//In a laboratory, n wizards must brew m potions in order. 
+//Each potion has a mana capacity mana[j] and must pass through all the wizards sequentially to be brewed properly. 
+//The time taken by the ith wizard on the jth potion is timeij = skill[i] * mana[j].
+//Since the brewing process is delicate, a potion must be passed to the next wizard immediately after the current wizard completes their work. 
+//This means the timing must be synchronized so that each wizard begins working on a potion exactly when it arrives. ​
+//Return the minimum amount of time required for the potions to be brewed properly.
+
+class Solution {
+    public long minTime(int[] skill, int[] mana) {
+        int n = skill.length;
+        int m = mana.length;
+
+        long[] f = new long[n];
+        
+        for (int j = 0; j < m; j++) {
+            long x = mana[j];
+            long current = f[0]; 
+            
+            for (int i = 1; i < n; i++) {
+                current = Math.max(current + (long) skill[i - 1] * x, f[i]);
+            }
+            
+            f[n - 1] = current + (long) skill[n - 1] * x;
+            
+            for (int i = n - 2; i >= 0; i--) {
+                f[i] = f[i + 1] - (long) skill[i + 1] * x;
+            }
+        }
+        
+        return f[n - 1];
+    }
+}

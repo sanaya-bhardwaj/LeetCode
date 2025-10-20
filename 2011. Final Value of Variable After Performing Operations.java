02a0b4fc36b69f76bb81d074a1f4924c5ae49940
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-10-20 17:36:22 +0530

    Create 2011. Final Value of Variable After Performing Operations.java

---
+++ 2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,16 @@
+//There is a programming language with only four operations and one variable X:
+//++X and X++ increments the value of the variable X by 1.
+//--X and X-- decrements the value of the variable X by 1.
+//Initially, the value of X is 0.
+//Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X = 0;
+        for (String op : operations) {
+            if (op.indexOf('+') != -1) X++;
+            else X--;
+        }
+        return X;
+    }
+}

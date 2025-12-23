ba9ad8260536b631a1722b557465b5a3845e4d57
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-12-24 00:49:48 +0530

    Update 189. Rotate Array.java

--- 189. Rotate Array.java
+++ 189. Rotate Array.java
@@ -16,3 +16,27 @@ class Solution {
         }        
     }
 }
+
+
+//2nd solution
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        Reverse(nums, 0, n-1);
+        Reverse(nums, 0, k-1);
+        Reverse(nums, k, n-1);
+    }
+
+    private void Reverse(int[] arr, int i, int j){
+        while(i<j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+        }
+    }
+}

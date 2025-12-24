bfb33a96376590741234686a0410017878d65b4c
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-12-24 18:31:23 +0530

    Update 283. Move Zeroes.java

--- 283. Move Zeroes.java
+++ 283. Move Zeroes.java
@@ -4,15 +4,42 @@
 
 class Solution {
     public void moveZeroes(int[] nums) {
-     int a=0;
-     for(int i=0;i<nums.length;i++){
-        if(nums[i]!=0){
-            nums[a]=nums[i];
-            a++;
-        } 
-     }
-     for(int i=a;i<nums.length;i++){
-        nums[i]=0;
-     }
+        int n = nums.length;
+        int j = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if (j == -1) return; // ⭐ important fix
+
+        for (int i = j + 1; i < n; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
     }
 }
+
+//2nd solution
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
+

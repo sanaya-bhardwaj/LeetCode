292f1979cd937016ae352c41d0d3b16959f6d4c9
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-12-26 18:25:07 +0530

    Create 2483. Minimum Penalty for a Shop.java

---
+++ 2483. Minimum Penalty for a Shop.java
@@ -0,0 +1,27 @@
+//You are given the customer visit log of a shop represented by a 0-indexed string customers consisting only of characters 'N' and 'Y':
+//if the ith character is 'Y', it means that customers come at the ith hour
+//whereas 'N' indicates that no customers come at the ith hour.
+//If the shop closes at the jth hour (0 <= j <= n), the penalty is calculated as follows:
+//For every hour when the shop is open and no customers come, the penalty increases by 1.
+//For every hour when the shop is closed and customers come, the penalty increases by 1.
+//Return the earliest hour at which the shop must be closed to incur a minimum penalty.
+
+class Solution {
+    public int bestClosingTime(String customers) {
+        int bestTime = 0;
+        int minPenalty = 0;
+        int prefix = 0;
+        
+        for (int i = 0; i < customers.length(); i++) {
+            char ch = customers.charAt(i);
+            prefix += ch == 'Y' ? -1 : 1;
+            
+            if (prefix < minPenalty) {
+                bestTime = i + 1;
+                minPenalty = prefix;
+            }
+        }
+        
+        return bestTime;
+    }
+}

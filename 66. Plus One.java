e84f3426a63b8553de343d3e6a21335caee01e8c
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-01-01 08:42:26 +0530

    Update 66. Plus One.java

--- 66. Plus One.java
+++ 66. Plus One.java
@@ -35,3 +35,17 @@
 //This solution iterates through the digits from right to left, incrementing each digit and checking for a carry. 
 //If there is a carry, it sets the current digit to 0 and continues the loop.
 //If there's still a carry after the loop, it means a new digit needs to be added at the beginning of the array.
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        final int n=digits.length;
+        for(int i=n-1; i>=0; i--){
+            digits[i]++;
+            if (digits[i]<10) return digits;
+            digits[i]=0;
+        }
+        digits=new int[n+1];
+        digits[0]=1;
+        return digits;
+    }
+}

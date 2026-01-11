828ed987e03b4b4d0f03f23eee8818e819498e71
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-01-11 22:35:57 +0530

    Update 85. Maximal Rectangle.java

--- 85. Maximal Rectangle.java
+++ 85. Maximal Rectangle.java
@@ -1,4 +1,5 @@
 //Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+
 class Solution {
   public int maximalRectangle(char[][] matrix) {
     if (matrix.length == 0)

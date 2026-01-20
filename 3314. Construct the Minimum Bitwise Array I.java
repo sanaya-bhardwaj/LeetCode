e188abf814c8d04ae8d27fa6ca562b64d2b3078f
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-01-20 14:24:07 +0530

    Update 3314. Construct the Minimum Bitwise Array I.java

--- 3314. Construct the Minimum Bitwise Array I.java
+++ 3314. Construct the Minimum Bitwise Array I.java
@@ -1,3 +1,8 @@
+/*You are given an array nums consisting of n prime integers.
+You need to construct an array ans of length n, such that, for each index i, the bitwise OR of ans[i] and ans[i] + 1 is equal to nums[i], i.e. ans[i] OR (ans[i] + 1) == nums[i].
+Additionally, you must minimize each value of ans[i] in the resulting array.
+If it is not possible to find such a value for ans[i] that satisfies the condition, then set ans[i] = -1.*/
+
 class Solution {
     public int[] minBitwiseArray(List<Integer> nums) {
         int[] res = new int[nums.size()];

550af37919278d4187851560a1a0010d6c9621f3
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-02-09 15:37:07 +0530

    Update 1382. Balance a Binary Search Tree.java

--- 1382. Balance a Binary Search Tree.java
+++ 1382. Balance a Binary Search Tree.java
@@ -1,7 +1,6 @@
-//Given the root of a binary search tree, return a balanced binary search tree with the same node values. 
-//If there is more than one answer, return any of them.
-
-//A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.
+/*Given the root of a binary search tree, return a balanced binary search tree with the same node values. 
+If there is more than one answer, return any of them.
+A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.*/
 
 /**
  * Definition for a binary tree node.

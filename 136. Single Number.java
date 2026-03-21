612cda7649cfd20a3ed624306f813583c45f4d23
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-03-21 22:02:41 +0530

    Refactor comments and add XOR implementation
    Updated comments for clarity and added a second implementation of the singleNumber method using XOR.

--- 136. Single Number.java
+++ 136. Single Number.java
@@ -1,5 +1,5 @@
-//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-//You must implement a solution with a linear runtime complexity and use only constant extra space.
+/*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.*/
 
 public class Solution {
     public int singleNumber(int[] nums) {
@@ -8,5 +8,17 @@ public class Solution {
             result ^= num;
         }
         return result;
+    }
+}
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            xor=xor^nums[i];
+        }
+        return xor;
     }
 }

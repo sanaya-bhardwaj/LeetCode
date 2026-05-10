698b460c5c72ea6afdf480604a41e8b139d3fce3
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-05-11 00:20:01 +0530

    Refactor maxSubArray to use Kadane's Algorithm
    Implemented Kadane's Algorithm to find the maximum subarray sum efficiently.

--- 53. Maximum Subarray.java
+++ 53. Maximum Subarray.java
@@ -1,6 +1,5 @@
 //Given an integer array nums, find the subarray with the largest sum, and return its sum.
 
-
 class Solution {
     public int maxSubArray(int[] nums) {
         int res = nums[0];
@@ -16,5 +15,37 @@ class Solution {
         }
 
         return res;        
+    }
+}
+
+//Best Approach using Kadane's Algorithm 
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // Maximum sum
+        long maxi = Long.MIN_VALUE; 
+        
+        // Current sum of subarray 
+        long sum = 0; 
+        
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            
+            // Add current element to the sum
+            sum += nums[i]; 
+            
+            // Update maxi if current sum is greater
+            if (sum > maxi) {
+                maxi = sum; 
+            }
+            
+            // Reset sum to 0 if it becomes negative
+            if (sum < 0) {
+                sum = 0; 
+            }
+        }
+        
+        // Return the maximum subarray sum found
+        return (int) maxi;
     }
 }

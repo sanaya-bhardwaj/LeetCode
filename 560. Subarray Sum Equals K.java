4ed74607751add3fc31d8c1b451659c912b81d3f
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-05-19 20:38:26 +0530

    Update 560. Subarray Sum Equals K.java

--- 560. Subarray Sum Equals K.java
+++ 560. Subarray Sum Equals K.java
@@ -1,6 +1,7 @@
 /*Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
 A subarray is a contiguous non-empty sequence of elements within an array.*/
 
+//Brute Force
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int count =0;

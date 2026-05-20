a62f693747c20fed838040361a506760b4a5046a
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-05-20 22:03:47 +0530

    Implement method to find Pascal's Triangle element
    Added a Solution class to compute the (r, c) element of Pascal's Triangle using the binomial coefficient formula.

--- 118. Pascal's Triangle.java
+++ 118. Pascal's Triangle.java
@@ -22,3 +22,23 @@ class Solution {
         return prevRows;
     }
 }
+
+// Solution class to find the (r, c) element of Pascal's Triangle
+class Solution {
+    // Function to compute binomial coefficient (nCr)
+    public long findPascalElement(int r, int c) {
+        // Element is C(r-1, c-1)
+        int n = r - 1;
+        int k = c - 1;
+
+        long result = 1;
+
+        // Compute C(n, k) using iterative formula
+        for (int i = 0; i < k; i++) {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+
+        return result;
+    }
+}

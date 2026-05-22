3973645f1838b55bcaebeb7e04ea6b8599a15876
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-05-23 02:32:38 +0530

    Add method comment for threeSum functionality
    Added a comment explaining the purpose of the threeSum method.

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -1,3 +1,6 @@
+/*Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.*/
+
 class Solution {
   public List<List<Integer>> threeSum(int[] nums) {
     if (nums.length < 3)

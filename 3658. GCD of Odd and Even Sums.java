d4609ae2881766957687987787bc0ac70e133d6b
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-07-16 02:15:02 +0530

    Add GCD computation for odd and even sums
    Implement a method to compute the GCD of sums of the smallest n positive odd and even numbers.

---
+++ 3658. GCD of Odd and Even Sums.java
@@ -0,0 +1,10 @@
+/*You are given an integer n. Your task is to compute the GCD (greatest common divisor) of two values:
+sumOdd: the sum of the smallest n positive odd numbers.
+sumEven: the sum of the smallest n positive even numbers.
+Return the GCD of sumOdd and sumEven.*/
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+            return n;
+    }
+}

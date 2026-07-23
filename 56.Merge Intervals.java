b5640bc0c719e11b7ca826811b925f0974807b7e
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-07-24 00:20:37 +0530

    Add optimal merge intervals solution
    Implemented an optimal solution for merging intervals using sorting and a list to store merged intervals.

--- 56.Merge Intervals.java
+++ 56.Merge Intervals.java
@@ -24,4 +24,25 @@ class Solution {
     }
 }
 
+//optimal
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> ans = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+
+            if (ans.isEmpty() || ans.get(ans.size() - 1)[1] < interval[0]) {
+                ans.add(new int[]{interval[0], interval[1]});
+            } else {
+                ans.get(ans.size() - 1)[1] =
+                    Math.max(ans.get(ans.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
  

3bbae4f2bdc3be8998d3b0e75fe6192f4b4bc89b
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-07-26 19:35:26 +0530

    Add solution for maximum product of three numbers
    Implement solution to find maximum product of three numbers in an array.

---
+++ 628. Maximum Product of Three Numbers.java
@@ -0,0 +1,13 @@
+/*Given an integer array nums, find three numbers whose product is maximum and return the maximum product.*/
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int result1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+        int result2 = nums[0] * nums[1] * nums[n - 1];
+
+        return Math.max(result1, result2);
+    }
+}

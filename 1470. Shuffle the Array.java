df4d75346051d890861d4355028b4cedb33fe279
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2026-07-26 20:11:54 +0530

    Create 1470. Shuffle the Array.java

---
+++ 1470. Shuffle the Array.java
@@ -0,0 +1,16 @@
+/*Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].*/
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans[j++] = nums[i];
+            ans[j++] = nums[i + n];
+        }
+
+        return ans;
+    }
+}
